509ba5a23b17cd59c0ffc23d3ca5d1a5384164be
zkkaa <muhammadazkafakhri0@gmail.com>
2026-03-28 23:53:43 +0700

    add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
+import { useRouter } from "next/navigation"; 
+
 export default function Home() {
+  const router = useRouter();
+
+  function handleGameDuelClick() {
+    router.push("/games/duel");
+  }
+
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
+    <div className="flex flex-col flex-1 items-center justify-center">
       <p>Kenalin Gweh Azka</p>
+      <button 
+        className="px-2 py-1 bg-pink-300 rounded-2xl" 
+        onClick={handleGameDuelClick}
+      >
+        Game duel
+      </button>
     </div>
   );
 }
