5690ab404e7d5672bc355889aaafdb1cf20dfe1d
zkkaa <muhammadazkafakhri0@gmail.com>
2026-03-30 16:40:59 +0700

    inisiasi materi

---
+++ src/app/materi/page.tsx
@@ -0,0 +1,7 @@
+export default function MateriPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-400 to-pink-400">
+            <h1 className="text-4xl font-bold text-white">Materi Coming Soon!</h1>
+        </div>
+    )
+}   
