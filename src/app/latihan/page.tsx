85482bfdd334d339bafc0d27275cf37325c204b7
zkkaa <muhammadazkafakhri0@gmail.com>
2026-03-30 16:42:28 +0700

    inisiasi latihan

---
+++ src/app/latihan/page.tsx
@@ -0,0 +1,7 @@
+export default function LatihanPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-green-400 to-blue-400">
+            <h1 className="text-4xl font-bold text-white">Latihan Coming Soon!</h1>
+        </div>
+    )
+}
